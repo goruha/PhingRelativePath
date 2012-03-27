434605d313f4f35084f1c88e7d8c062bf9b3bef4
Rodonov Igor <goruha@gmail.com>
2012-03-27 14:10:41 +0700

    add rel links

--- PhingRelativePath.php
+++ PhingRelativePath.php
@@ -3,31 +3,34 @@
 require_once "phing/tasks/ext/SymlinkTask.php";
 class PhingRelativePath extends SymlinkTask {
   private function getRelativePath($from, $to) {
-    $from = explode('/', $from);
-    $to = explode('/', $to);
-    $relPath = $to;
+   // add because we use it as base dir
+   $from = realpath($from)."/";
+   $to = realpath($to);
+   $from = explode('/', $from);
+   $to = explode('/', $to);
+   foreach($from as $depth => $dir)
+   {
 
-    foreach ($from as $depth => $dir) {
-      // find first non-matching dir
-      if ($dir === $to[$depth]) {
-        // ignore this directory
-        array_shift($relPath);
-      }
-      else {
-        // get number of remaining dirs to $from
-        $remaining = count($from) - $depth;
-        if ($remaining > 1) {
-          // add traversals up to first matching dir
-          $padLength = (count($relPath) + $remaining - 1) * -1;
-          $relPath = array_pad($relPath, $padLength, '..');
-          break;
+        if(isset($to[$depth]))
+        {
+            if($dir === $to[$depth])
+            {
+               unset($to[$depth]);
+               unset($from[$depth]);
+            }
+            else
+            {
+               break;
+            }
         }
-        else {
-          $relPath[0] = './' . $relPath[0];
-        }
-      }
     }
-    return implode('/', $relPath);
+    //$rawresult = implode('/', $to);
+    for($i=0;$i<count($from)-1;$i++)
+    {
+        array_unshift($to,'..');
+    }
+    $result = implode('/', $to);
+    return $result;
   }
 
     /**
@@ -54,12 +57,10 @@ class PhingRelativePath extends SymlinkTask {
             unlink($link);
         }
 
-        $fs = FileSystem::getFileSystem();
-
         $this->log('Linking: ' . $target . ' to ' . $link, Project::MSG_INFO);
-
         $base_dir = dirname($link);
-        $link = $this->getRelativePath($link, $target);
-        return exec("cd $base_dir; ln -s $target $link");
+        $link = basename($link);
+        $rel_path = $this->getRelativePath($base_dir, $target);
+        return exec("cd $base_dir; ln -s $rel_path $link");
     }
 }
